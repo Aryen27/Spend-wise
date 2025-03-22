f6026bb1354c4c607dcc81e4e8a2cc934611bfac
Aryan Rajeev <raghavaan27@gmail.com>
2025-03-23 01:13:46 +0530

    Connected DB to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const dbKey = import.meta.env.VITE_DB_KEY;
+const supabase = createClient("https://xbpvzcaibzouvbsajtny.supabase.co", dbKey);
 
 function App() {
+  const [instruments, setInstruments] = useState([]);
+  
+  useEffect(() => {
+      getInstruments();
+  }, []);
 
-  return (
-    <>
-      <h1>Hello!</h1>
-    </>
-  )
+  async function getInstruments() {
+    const { data } = await supabase.from("users").select(); //Table name
+    console.log(data);
+    setInstruments(data);
+  }
+    return (
+      <ul>
+        {instruments.map((instrument) => (
+          <li key={instrument.name}>{instrument.name}</li>
+        ))}
+      </ul>
+    );
 }
-
 export default App
