c2d7d7626fedafa42f3d21f5a2c63d7c64f3e7af
Aryan Rajeev <raghavaan27@gmail.com>
2025-03-23 18:47:17 +0530

    Added and configured shadcn and tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
-import {  useUsers } from "./contexts/dbContext";
+import { useUsers } from "./contexts/dbContext";
+import DashPage from "./dashboard/DashPage";
 
 function App() {
   const { users } = useUsers();
-  console.log(users[0]);
+  console.log(users);
   return (
     <>
-      <ul>
-        {/* {users.map((user) => (
+      {/* <ul>
+        {users.map((user) => (
           <li key={user.name}>{user.name} , {user.email }</li>
-        ))} */}
-
-      </ul>
+        ))}
+      </ul> */}
+      <DashPage/>
       </>
     );
 }
