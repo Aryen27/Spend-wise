9721d60da1e7e92d3be8d5372c029fbfcf25fac5
Aryan Rajeev <raghavaan27@gmail.com>
2025-03-23 20:50:53 +0530

    Added function to fetch transaction data

--- src/transactions/TransactionLayout.jsx
+++ src/transactions/TransactionLayout.jsx
@@ -4,7 +4,11 @@ import { useQuery } from '@tanstack/react-query'
 import { getTransactions } from "../services/apiTransactions";
 
 function TransactionLayout() {
-
+  const query= useQuery({
+    queryKey: ['tid'],
+    queryFn: getTransactions
+  });
+  console.log(query.data);
   return (
     <div>TransactionLayout</div>
   )
